b84c12768e3cb427be275b95e2bc4acd234e3974
AbdulrahmanKha4 <>
2021-06-03 12:52:37 +0000

    java

---
+++ Script.js
@@ -0,0 +1,41 @@
+var name = prompt("what is your name?")
+alert(name )
+
+
+var password = "1234"
+
+
+
+var  password = prompt("type the password ? ");
+
+
+
+
+if (password == "1234") {
+
+  alert("Welcome")
+
+} else  {
+
+  alert("you have one chance")
+}
+
+
+
+var who = "Killer"
+
+var who = prompt("who I am?");
+
+
+if (who == "killer") {
+
+  alert("Welcome")
+} else {
+
+  alert(" You will be killed" )
+  
+}
+
+
+
+document.write("Hello World!");
